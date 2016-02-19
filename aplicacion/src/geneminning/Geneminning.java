3fa81ee03f7525fc2dfe686642077d4ccd507bd6
Halcon <luishg@uaeh.edu.mx>
2016-02-19 16:03:30 -0600

    Aplicacion con interacción de archivos lee el archivo "archivo.txt" genera los archivos archivoFormateado.txt estadisticas.txt

--- aplicacion/src/geneminning/Geneminning.java
+++ aplicacion/src/geneminning/Geneminning.java
@@ -21,16 +21,32 @@ public class Geneminning {
     public static void main(String[] args) {
       FileLoader fl=new FileLoader("","archivo.txt");
       TextProcessor tp=new TextProcessor(fl.getText());
-      System.out.println("\ntexto Formateado:longitud=" + tp.getLongitud()+ ",dimeros=" + tp.getNumeroDimeros() + "\n" + tp.getTextoFormateado());
-      System.out.println("dimeros:");
+      
+      
+      FileLoader fl1=new FileLoader("", "archivoFormateado.txt");
+      fl1.setContenidoArchivo(tp.getTextoFormateado());
+      fl1.saveText();
+      
+      String textoGuardar;
+      textoGuardar="\ntexto Formateado:longitud=" + tp.getLongitud()+ ",dimeros=" + tp.getNumeroDimeros() + "\ndimeros:\n";
+      
       for(String dimero:tp.getListadoDimeros()){
-          System.out.println(dimero);
+          textoGuardar+=dimero + ",\n";
       }
-      System.out.println("\nFrecuencia de los dimeros:");
+      textoGuardar=textoGuardar.substring(0, textoGuardar.length()-2);
+      textoGuardar+="\nFrecuencia de los dimeros:\n";
       for(Dimero dimero:tp.getFrecuenciaDimeros()){
-          System.out.println(dimero.getDimero() + "\t" + dimero.getCantidad());
+          textoGuardar+=dimero.getDimero() + "\t" + dimero.getCantidad() + ",\n";
       }
-      fl.setContenidoArchivo(tp.getTextoFormateado());
-      fl.saveText();
+      textoGuardar=textoGuardar.substring(0, textoGuardar.length()-2);
+      FileLoader fl2=new FileLoader("", "estadisticas.txt");
+      fl2.setContenidoArchivo(textoGuardar);
+      fl2.saveText();
+      
+      
+      System.out.println("Press any key to continue...");
+      try{
+            System.in.read();
+       }catch(Exception e){}  
     }
 }
