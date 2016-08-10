ec6757eaf2cab3abee58120b6a97baed4cc4c590
Luis <Luis@Luis-PC>
2016-08-10 13:38:46 -0500

    Nueva version con lectura de XML

--- src/test/ArchivoXML.java
+++ src/test/ArchivoXML.java
@@ -1,6 +1,6 @@
 package test;
 
-import mx.edu.uaeh.files.XMLLoader;
+import mx.edu.uaeh.files.XMLParser;
 
 /**
  *
@@ -8,7 +8,7 @@ import mx.edu.uaeh.files.XMLLoader;
  */
 public class ArchivoXML {
     public static void main(String... args){
-        XMLLoader xmlLoader=new XMLLoader("secuencia.xml");
+        XMLParser xmlLoader=new XMLParser("secuencia.xml");
         xmlLoader.loadFile();
     }
 }
